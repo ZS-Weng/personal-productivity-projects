ac44c85222c0defa7ddf989ca286f124f25c5cdd
ZS-Weng <108080619+ZS-Weng@users.noreply.github.com>
2026-03-23 21:32:42 +0800

    Updated scripts

--- agentic_research/main.py
+++ agentic_research/main.py
@@ -2,11 +2,19 @@
 
 from config import DEFAULT_MODEL
 from llm_client import generate_response
+from tools.web_scraper import fetch_page, fetch_page_to_md
 
-def main():
-    prompt = "What is the origin of the Python programming language?"
+def summarize_page():
+    url = "https://www.mot.gov.sg/news-resources/newsroom/speech-by-acting-minister-for-transport-mr-jeffrey-siow-at-ministry-of-transport-s-committee-of-supply-debate-2026/"
+    background = fetch_page(url)
+    prompt = f"Based on the following text: {background},\n\nsummarize on the main points and key takeaways."
     response = generate_response(prompt)
     print(response)
 
+def html_to_md():
+    url = "https://www.mot.gov.sg/news-resources/newsroom/speech-by-acting-minister-for-transport-mr-jeffrey-siow-at-ministry-of-transport-s-committee-of-supply-debate-2026/"
+    fetch_page_to_md(url, subfolder="budget_2026")
+    
 if __name__ == "__main__":
-    main()
+    # summarize_page()
+    html_to_md()
--- agentic_research/tools/web_scraper.py
+++ agentic_research/tools/web_scraper.py
@@ -1,8 +1,24 @@
 import httpx
 from bs4 import BeautifulSoup
+import html2text
+import os
+from pathlib import Path
 
 def fetch_page(url):
     response = httpx.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     text = soup.get_text(separator="\n", strip=True)   
     return text
+
+def fetch_page_to_md(url, subfolder=None):
+    h = html2text.HTML2Text()
+    response = httpx.get(url)
+    markdown = h.handle(response.text)
+    base_path = Path("../research")
+    if subfolder:
+        os.makedirs(f"../research/{subfolder}", exist_ok=True)
+        filename = base_path/f"{subfolder}/{url.split('/')[-2]}.md"
+    else:
+        filename = base_path/f"{url.split('/')[-2]}.md"
+    with open(filename, "w") as f:
+        f.write(markdown)
